ecaafca4bfce0367636a24532f0ded30f255cce9
Aayush Pandey <139016137+AayushPandey05@users.noreply.github.com>
2026-02-04 23:36:41 +0530

    Class Exercise

--- Exercises/reverse_Linked_List.cpp
+++ Exercises/reverse_Linked_List.cpp
@@ -1,46 +1,56 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode* next;
-
-    ListNode(int x) {
-        val = x;
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
         next = NULL;
     }
 };
 
-ListNode* reverseList(ListNode* head) {
-    ListNode* prev = NULL;
-    ListNode* curr = head;
+Node *reverseLL(Node *head){
+    Node *curr = head;
+    Node *prev = NULL;
+    Node *next;
 
-    while (curr != NULL) {
-        ListNode* next = curr->next;
-        curr->next = prev;
+    while(curr != NULL){
+        next = curr -> next;
+        curr -> next = prev;
+
         prev = curr;
         curr = next;
     }
     return prev;
 }
 
-void printList(ListNode* head) {
-    while (head != NULL) {
-        cout << head->val << " ";
-        head = head->next;
+void print(Node *head){
+    Node *curr = head;
+    while(curr != NULL){
+        cout << curr -> data << " ";
+        curr = curr -> next;
     }
-    cout << endl;
+    cout << "NULL";
 }
 
-int main() {
-    ListNode* head = new ListNode(1);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(3);
-    head->next->next->next = new ListNode(4);
 
-    printList(head);
-    head = reverseList(head);
-    printList(head);
+int main(){
+    Node *head = new Node(10);
+    head -> next = new Node(20);
+    head -> next -> next = new Node(30);
+    head -> next -> next -> next = new Node(40);
+    head -> next -> next -> next -> next = new Node(50);
+
+    cout << "Original: ";
+    print(head);
+
+    head = reverseLL(head);
+
+    cout << endl;
+
+    cout << "Reversed: ";
+    print(head);
 
     return 0;
 }

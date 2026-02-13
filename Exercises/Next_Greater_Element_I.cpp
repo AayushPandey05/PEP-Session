df09ff96a11f7337dccc86cc0ad040dae60ee7f0
Aayush Pandey <139016137+AayushPandey05@users.noreply.github.com>
2026-02-13 10:20:25 +0530

    Class Exercise

--- Exercises/Next_Greater_Element_I.cpp
+++ Exercises/Next_Greater_Element_I.cpp
@@ -1,8 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2){
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
 
+    unordered_map<int, int> mp;   // element -> next greater
+    stack<int> st;
+
+    for(int i = 0; i < nums2.size(); i++){
+
+        while(!st.empty() && nums2[i] > st.top()){
+            mp[st.top()] = nums2[i];
+            st.pop();
+        }
+
+        st.push(nums2[i]);
+    }
+
+    // elements left in stack have no next greater
+    while(!st.empty()){
+        mp[st.top()] = -1;
+        st.pop();
+    }
+
+    vector<int> ans;
+    for(int x : nums1){
+        ans.push_back(mp[x]);
+    }
+    return ans;
 }
 
 int main(){
@@ -19,11 +43,11 @@ int main(){
 
     vector<int> nums2(m);
     for(int i = 0; i < m; i++){
-        cin >> nums2[m];
+        cin >> nums2[i];
     }
 
     vector<int> res = nextGreaterElement(nums1, nums2);
-    for(int x :  res){
+    for(int x : res){
         cout << x << " ";
     }
 
